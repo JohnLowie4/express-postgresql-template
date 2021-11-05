326fb86a2cb269596083c4b15ba1233ac9d9e843
John <jzlowie@gmail.com>
2021-11-04 21:52:35 -0600

    fixed errors in table creation, seeded some data

---
+++ src/db/seeds/01_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (first_name, last_name, email) VALUES ('Tony', 'Stark', 'tony.stark@starkindustry.com');
+INSERT INTO users (first_name, last_name, email) VALUES ('Peter', 'Parker', 'peter.parker@midtown.com');
