ec91a57457e8540c41ea059bc55f17522c44e2dd
John <jzlowie@gmail.com>
2021-11-05 22:21:19 -0600

    seeded all template data

---
+++ src/db/seeds/02_user_phone_number.sql
@@ -0,0 +1,3 @@
+INSERT INTO user_phone_number (user_id, phone_number, number_type) VALUES (1, '1112223333', 'primary');
+INSERT INTO user_phone_number (user_id, phone_number, number_type) VALUES (1, '4445556666', 'secondary');
+INSERT INTO user_phone_number (user_id, phone_number, number_type) VALUES (2, '1234567890', 'primary');
---
+++ src/db/seeds/03_user_account.sql
@@ -0,0 +1,5 @@
+INSERT INTO user_account (user_id, account_type, interest_rate) VALUES (1, 'savings', 0.1);
+INSERT INTO user_account (user_id, account_type, interest_rate) VALUES (1, 'TFSA', 2.5);
+INSERT INTO user_account (user_id, account_type, interest_rate) VALUES (1, 'RRSP', 3.2);
+INSERT INTO user_account (user_id, account_type, interest_rate) VALUES (2, 'checking', 0.01);
+INSERT INTO user_account (user_id, account_type, interest_rate) VALUES (2, 'savings', 0.05);
