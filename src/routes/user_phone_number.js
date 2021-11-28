6f0fc4479928dbb2af37920a2d7e0fe6225e3f4b
John <jzlowie@gmail.com>
2021-11-28 16:36:50 -0700

    delete phone number is successful

--- src/routes/user_phone_number.js
+++ src/routes/user_phone_number.js
@@ -82,6 +82,21 @@ const user_phone_number = (db) => {
       });
   });
 
+  route.delete("/delete/:id/:user_id", (req, res) => {
+    db.query(
+      `
+        DELETE FROM user_phone_number WHERE id=$1 AND user_id=$2
+      `,
+      [req.params.id, req.params.user_id]
+    )
+      .then((response) => {
+        res.json(response.rows[0]);
+      })
+      .catch((error) => {
+        res.json(error.message);
+      });
+  });
+
   return route;
 };
 
